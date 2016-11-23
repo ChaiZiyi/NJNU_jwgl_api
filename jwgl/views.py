08c83e92eb52d3ad002151568fdaf8e90bf06d46
crazyczy <crazyczy@live.cn>
2016-11-23 13:41:37 +0800

    1.1
    修改了返回不是json格式的错误，添加了两个遗忘的可能情况

--- jwgl/views.py
+++ jwgl/views.py
@@ -10,21 +10,21 @@ def index(request):
 def info(request):
     if request.method == 'GET':
         cookies = request.COOKIES
-        return HttpResponse(apis.getInfo(cookies))
-    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')))
+        return HttpResponse(apis.getInfo(cookies), content_type="application/json")
+    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')),content_type="application/json")
 
 def login(request):
     if request.method == 'GET':
         username = request.GET.get('username','')
         password = request.GET.get('password','')
         result = apis.login(username,password)
-        response = HttpResponse(result)
+        response = HttpResponse(result,content_type="application/json")
         try:
             response.set_cookie('JSESSIONID',json.loads(result)['cookies']['JSESSIONID'])
         except:
             pass
         return response
-    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')))
+    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')),content_type="application/json")
 
 def scores(request):
     if request.method == 'GET':
@@ -32,16 +32,16 @@ def scores(request):
         sjxz = request.GET.get('sjxz',1)
         xn = request.GET.get('xn',2016)
         xq = request.GET.get('xq',0)
-        return HttpResponse(apis.getScore(cookies,sjxz,xn,xq))
-    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')))
+        return HttpResponse(apis.getScore(cookies,sjxz,xn,xq), content_type="application/json")
+    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')),content_type="application/json")
 
 def schedule(request):
     if request.method == 'GET':
         cookies = request.COOKIES
         xn = request.GET.get('xn',2016)
         xq = request.GET.get('xq',0)
-        return HttpResponse(apis.getSchedule(cookies,xn,xq))
-    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')))
+        return HttpResponse(apis.getSchedule(cookies,xn,xq),content_type="application/json")
+    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')),content_type="application/json")
 
 def others(request):
-    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')))
+    return HttpResponse(json.dumps(dict(status='404',message=u'你来到了没有数据的荒原')),content_type="application/json")
